e942cfb999313540b8ea2ae66c71b2d1ea85487d
Goodman Luphondo <luphondog@outlook.com>
2020-10-31 17:04:24 +0200

    dist folder

---
+++ dist/precache-manifest.c169ece858d1a96dd63f32020363c1bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472dccb4f54655ef4756",
+    "url": "/css/app.61378bde.css"
+  },
+  {
+    "revision": "9919c02eca8f3c23d6b504b93b1f1776",
+    "url": "/img/cheerful.9919c02e.jpg"
+  },
+  {
+    "revision": "0d3efcab1f19fa354976afa12998a0ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "472dccb4f54655ef4756",
+    "url": "/js/app.547e7cb7.js"
+  },
+  {
+    "revision": "e1052727f28791488a71",
+    "url": "/js/chunk-vendors.b08775bb.js"
+  },
+  {
+    "revision": "b2a4b0139f2e3a785d1a050e87eb50f8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
